81bf0c6e0cd1d627cccdc1135aad23087643eaee
Alan Vale <valealan84@gmail.com>
2023-12-10 23:14:05 -0300

    Add files via upload

--- script.c
+++ script.c
@@ -1,14 +1,22 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
+#include <time.h>
 
 #define FILE_NAME "me.ppm"
 #define R_MUL 1
 #define G_MUL 1
 #define B_MUL 1
+#define MIRROR 0
+#define UPSIDE_DOWN 0
+#define INVERT 0
+#define RANDOMIZE 1
+#define RANDOM_OFFSET 5
 
 // ---
 
 int main() {
+  srand(time(0));
+
   SDL_Window* win;
   SDL_Renderer* ren;
   
@@ -33,11 +41,22 @@ int main() {
     fscanf(img, "%d %d %d", &buffer[0], &buffer[1], &buffer[2]);
 
     int x = i % w;
+    if (MIRROR) { x = w - x; }
     int y = i / w;
+    if (UPSIDE_DOWN) { y = h - y; }
+    if (RANDOMIZE) {
+      x += random() % (RANDOM_OFFSET * 2) - RANDOM_OFFSET;
+      y += random() % (RANDOM_OFFSET * 2) - RANDOM_OFFSET;
+    }
 
     int r = buffer[0] * R_MUL;
     int g = buffer[1] * G_MUL;
     int b = buffer[2] * B_MUL;
+    if (INVERT) {
+      r = 255 - r;
+      g = 255 - g;
+      b = 255 - b;
+    }
 
     SDL_SetRenderDrawColor(ren, r, g, b, 255);
     SDL_RenderDrawPoint(ren, x, y);
